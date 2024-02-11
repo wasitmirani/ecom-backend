7694c1f02dab25cf8d4ec4eee4716c2ae113e6c6
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2024-02-11 16:54:48 +0500

    update: new buid added

--- resources/views/layouts/backend/master.blade.php
+++ resources/views/layouts/backend/master.blade.php
@@ -20,7 +20,7 @@
         <link href="{{ asset('/assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
 
 
-{{-- 
+{{--
         @viteReactRefresh
         @vite(['resources/js/index.tsx']) --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -41,7 +41,7 @@
                 const scriptList = [
                   '/assets/libs/simplebar/simplebar.min.js',
                  '/assets/libs/node-waves/waves.min.js',
-          
+
                  '/assets/libs/feather-icons/feather.min.js',
                  '/assets/js/pages/plugins/lord-icon-2.1.0.js',
                  '/assets/js/plugins.js',
@@ -58,7 +58,8 @@
                   }, index * 200); // Adjust the delay as needed
                 });
               </script>
-                  @vitereactrefresh()
+                 <script type="text/javascript" src="{{asset('/build/assets/index-53906c41.js')}}"></script>
+              
                   @vite(['resources/js/index.tsx'])
 
                   <script>
@@ -77,6 +78,6 @@
                    localStorage.setItem('permissions', JSON.stringify(permissions))
                    localStorage.removeItem('user');
                    localStorage.setItem('user', JSON.stringify(user))
-                </script>    
+                </script>
     </body>
 </html>
