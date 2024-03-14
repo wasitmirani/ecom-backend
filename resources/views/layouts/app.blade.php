1f20d14ef526db3402d96cacd35f87ab75d66fa7
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2024-03-14 15:29:32 +0500

    new build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{asset('/build/assets/index-9c4c082a.js')}}">
+        <link rel="stylesheet" href="{{asset('/build/assets/index-93d8b085.css')}}">
         <script type="text/javascript" src="{{asset('/build/assets/index-9c4c082a.js')}}"></script>
         <!-- Scripts -->
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
